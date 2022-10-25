3326f6c023836863cc1c21156b5079fd577cfe10
syed-irfan786 <syedirfan.bses19@iba-suk.edu.pk>
2022-10-25 09:46:36 +0500

    my first commit to github

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+       <h1>heelo this is testing file</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
